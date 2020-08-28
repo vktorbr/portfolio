5efbb09c2bd2b2ba49e2d824845cdd1fe8469286
vktorbr <vktorbr@gmail.com>
2020-08-28 15:32:30 -0300

    backend configurado

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+const server = express();
+
+server.use(express.static('public'));
+
+server.set('view engine', 'html');
+
+nunjucks.configure('views', {
+    express: server
+})
+
+//rotas
+server.get('/', function(req, res){
+    return res.render('index');
+})
+
+server.get('/portfolio', function(req, res){
+    return res.render('portfolio');
+})
+
+server.listen(5000, function(){
+    console.log('server is running');
+})
