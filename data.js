52d98ab0219d78c891bc303ac1b48ff3ad60dbb4
vktorbr <vktorbr@gmail.com>
2020-08-29 10:23:07 -0300

    passando dados do back para o front

---
+++ data.js
@@ -0,0 +1,41 @@
+module.exports = [
+    {
+        id: '1nVUfZg2dSA',
+        title: 'Como inicio meus apps com ReactJS? Next.js, TypeScript, ESLint e Styled Components | Code/Drops #50',
+        duration: '44 min',
+        price: 'Free',
+        featured: true
+    },
+    {
+        id: 'bJVp_vlvMwQ',
+        title: 'Interface da Twitch.tv | UI Clone',
+        duration: '59 min',
+        price: 'Free'
+    },
+    {
+        id: 'De5np8phQxo',
+        title: 'Armazenando dados no Navegador com localStorage | Code/Drops #49',
+        duration: '52 min',
+        price: 'Free',
+        featured: true
+    },
+    {
+        id: 'HKE4ukzOz-U',
+        title: 'Por que migramos para o Linux? Como testar apps iOS no Windows, GarageBand com RN? | #PR 15',
+        duration: '18 min',
+        price: 'Free'
+    },
+    {
+        id: 'oYk2A7t_x0o',
+        title: 'Marcos Mathias, Business Analyst, Project Manager, Team Lead at IBM | Alumni #07',
+        duration: '60 min',
+        price: 'Free'
+    },
+    {
+        id: '34fiafm5gz4',
+        title: 'Inteligência Artificial, por onde começar?',
+        duration: '62 min',
+        price: 'Free',
+        featured: true
+    },
+]
--- server.js
+++ server.js
@@ -2,22 +2,37 @@ const express = require('express');
 const nunjucks = require('nunjucks');
 
 const server = express();
+const videos = require('./data')
 
 server.use(express.static('public'));
 
 server.set('view engine', 'njk');
 
 nunjucks.configure('views', {
-    express: server
+    express: server,
+    autoescape: false,
+    noCache: true
 })
 
 //rotas
 server.get('/', function(req, res){
-    return res.render('about');
+    const about = {
+        avatar_url: 'https://avatars1.githubusercontent.com/u/33494087?s=460&u=8956e4ac4beec29c0e2b9c60c01f12d6472160c3&v=4',
+        name: 'VKtor Mrtins',
+        role: 'Natural Bodybuilder',
+        description: 'Programador full-stack, bodybuilder e data scientist',
+        links: [
+            { name: 'Github', url: 'https://github.com/vktorbr'},
+            { name: 'Twitter', url: 'https://twitter.com/vktorbr'},
+            { name: 'Instagram', url: 'https://instagram.com/victornelas'}
+        ]
+    }
+
+    return res.render('about', {about});
 })
 
 server.get('/portfolio', function(req, res){
-    return res.render('portfolio');
+    return res.render('portfolio', {items: videos});
 })
 
 server.listen(5000, function(){
