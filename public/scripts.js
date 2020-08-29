a9df5d8aa5b195b04da3f1dd06889e2fd8403a89
vktorbr <vktorbr@gmail.com>
2020-08-29 15:05:24 -0300

    pagina para os videos

--- public/scripts.js
+++ public/scripts.js
@@ -3,16 +3,7 @@ const cards = document.querySelectorAll('.card');
 
 for (let card of cards){
     card.addEventListener('click', function(){
-        modalOverlay.classList.add('active');
         const videoId = card.getAttribute('id');
-        modalOverlay.querySelector('iframe').src = `https://youtube.com/embed/${videoId}`;
+        window.location.href = `/video?id=${videoId}`;
     })
 }
-
-const closeButton = document.querySelector('.close-modal');
-
-closeButton.addEventListener('click', function(){
-    modalOverlay.classList.remove('active');
-    modalOverlay.querySelector('iframe').src = '';
-})
-
